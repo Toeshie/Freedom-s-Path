147cc6e42eeaef7ee21857bf8f18b6d6eab134db
Toeshie <2212613@my.ipleiria.pt>
2023-08-26 18:01:25 +0100

    Final Credit Scene
    Final Credit Scene

--- Assets/Scripts/LevelTransitionScripts/FinalCutSceneTrigger.cs
+++ Assets/Scripts/LevelTransitionScripts/FinalCutSceneTrigger.cs
@@ -10,7 +10,13 @@ public class FinalCutSceneTrigger : MonoBehaviour
     [SerializeField] private VideoPlayer videoPlayer = null;
     [SerializeField] private GameObject creditsFrame;
     [SerializeField] private Movement playerReference;
-
+    [SerializeField] private GameObject dedication;
+    [SerializeField] private GameObject role1;
+    [SerializeField] private GameObject role2;
+    [SerializeField] private GameObject gameProgrammer;
+    [SerializeField] private GameObject gameArtist;
+    [SerializeField] private GameObject creditsTitle;
+   
     private void OnTriggerEnter2D(Collider2D other)
     {
         if (other.gameObject.CompareTag("Player"))
@@ -24,14 +30,25 @@ public class FinalCutSceneTrigger : MonoBehaviour
 
     private IEnumerator BackToMainMenu()
     {
-        yield return new WaitForSeconds(15f);
+        yield return new WaitForSeconds(19f);
         SceneManager.LoadScene("Main Menu");
+    }
+
+    private IEnumerator Credits()
+    {
+        yield return new WaitForSeconds(7f);
+        role1.SetActive(true);
+        role2.SetActive(true);
+        gameArtist.SetActive(true);
+        gameProgrammer.SetActive(true);
+        creditsTitle.SetActive(true);
     }
 
     private IEnumerator CreditsTimer()
     {
-        yield return new WaitForSeconds(6f);
+        yield return new WaitForSeconds(5f);
         creditsFrame.SetActive(true);
-        
+        dedication.SetActive(true);
+        StartCoroutine(Credits());
     }
 }
